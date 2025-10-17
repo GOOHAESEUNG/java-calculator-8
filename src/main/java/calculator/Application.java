c4ab28a79f95e2955c8ac5dbcc0244070444ab35
GOOHAESEUNG <amu6675@naver.com>
2025-10-17 23:54:46 +0900

    feat(parer) : 구분자를 기준으로 문자열을 분리하는 클래스 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,14 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         String input = inputView.inputView();
+
+        StringSplitter splitter = new StringSplitter();
+        List<String> inputList = splitter.splitByDelimiter(input);
     }
 }
