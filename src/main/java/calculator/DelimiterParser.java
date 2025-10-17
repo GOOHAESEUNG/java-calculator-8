4c9d25d9ef25ee1ebb5f578585a4edab12c9ea68
GOOHAESEUNG <amu6675@naver.com>
2025-10-18 00:30:10 +0900

    feat(parser) : 커스텀 구분자 추출 기능 추가

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -1,7 +1,19 @@
 package calculator;
 
 public class DelimiterParser {
-    public void parseDelimiter(String input) {
+    public String parseDelimiter(String input) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int index = 0;
 
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) != '\n') {
+                stringBuffer.append(input.charAt(i));
+            } else {
+                index = i;
+                break;
+            }
+        }
+
+        return stringBuffer.toString();
     }
 }
