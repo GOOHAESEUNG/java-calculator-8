97fa525641c657ae8f2fd6e671789f60e09d0270
GOOHAESEUNG <amu6675@naver.com>
2025-10-18 21:19:56 +0900

    refact(splitter) : 기본 구분자로 분리 리팩토링

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -1,22 +1,11 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StringSplitter {
     public List<String> splitByDelimiter(String input) {
         input = trimInput(input);
-        List<String> inputList = new ArrayList<>();
-
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ',' || input.charAt(i) == ';') {
-                inputList.add(String.valueOf(input.charAt(i - 1)));
-                inputList.add(String.valueOf(input.charAt(i + 1)));
-            }
-        }
-
-        System.out.println(inputList);
-        return inputList;
+        return List.of((input.split("[,:]")));
     }
 
     public List<String> splitByCustomDelimiter(String customDelimiter, String input) {
